c6ec7eaf9ba5844b8ddb8070d088e293b8321047
Kritapas Nimtrakul <kxn9945@rit.edu>
2017-10-17 03:52:54 -0400

    A03 Deliverable

---
+++ A03 Deliverable/AppClass.cpp
@@ -0,0 +1,180 @@
+#include "AppClass.h"
+void Application::InitVariables(void)
+{
+	////Change this to your name and email
+	//m_sProgrammer = "Kritapas Nimtrakul - kxn9945@rit.edu";
+
+	////Alberto needed this at this position for software recording.
+	//m_pWindow->setPosition(sf::Vector2i(710, 0));
+	
+	//Set the position and target of the camera
+	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
+	m_pCameraMngr->SetPositionTargetAndUp(AXIS_Z * 20.0f, ZERO_V3, AXIS_Y);
+
+	//if the light position is zero move it
+	if (m_pLightMngr->GetPosition(1) == ZERO_V3)
+		m_pLightMngr->SetPosition(vector3(0.0f, 0.0f, 3.0f));
+
+	//if the background is cornflowerblue change it to black (its easier to see)
+	if (vector3(m_v4ClearColor) == C_BLUE_CORNFLOWER)
+	{
+		m_v4ClearColor = vector4(ZERO_V3, 1.0f);
+	}
+	
+	//if there are no segments create 7
+	if(m_uOrbits < 1)
+		m_uOrbits = 7;
+
+	float fSize = 1.0f; //initial size of orbits
+	
+	
+
+
+	//creating a color using the spectrum 
+	uint uColor = 650; //650 is Red
+	//prevent division by 0
+	float decrements = 250.0f / (m_uOrbits > 1? static_cast<float>(m_uOrbits - 1) : 1.0f); //decrement until you get to 400 (which is violet)
+	/*
+		This part will create the orbits, it start at 3 because that is the minimum subdivisions a torus can have
+	*/
+	uint uSides = 3; //start with the minimal 3 sides
+	for (uint i = uSides; i < m_uOrbits + uSides; i++)
+	{
+		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
+		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
+		fSize += 0.5f; //increment the size for the next orbit
+		uColor -= static_cast<uint>(decrements); //decrease the wavelength
+	}
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix(); //view Matrix
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix(); //Projection Matrix
+	matrix4 m4Offset = IDENTITY_M4; //offset of the orbits, starts as the global coordinate system
+	/*
+		The following offset will orient the orbits as in the demo, start without it to make your life easier.
+	*/
+	//m4Offset = glm::rotate(IDENTITY_M4, 90.0f, AXIS_Z);
+
+	static float fTimer = 0;	//store the new timer
+	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
+	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+	float fRadius = 0.95f;
+	float angle = 0;
+	uint uSides = 3;
+
+	// store all vertices
+	static std::vector<vector3> stoplist;
+	static bool bInit = false; //Initialize once
+	if (!bInit)
+	{
+		// loop through all object and calculate their vertices position
+		for (int i = 0;i < m_uOrbits;i++)
+		{
+			// start with translation fRadius
+			vector3 currentVector(fRadius, 0.0f, 0.0f);
+			for (int j = 0;j < uSides;j++)
+			{
+				// push the vertices into the list
+				stoplist.push_back(currentVector);
+				// calculate rotation of the next vertices base on how many side there is
+				angle = 360 / uSides;
+				matrix4 rot = glm::rotate(matrix4(1.0f), angle, 0.0f, 0.0f, 1.0f);
+				currentVector = vector3(rot * vector4(currentVector,0.0f));
+			}
+			uSides++;
+			fRadius = fRadius + 0.5f;
+		}
+
+		bInit = true;
+	}
+
+
+	//static uint route;
+	vector3 v3Start;
+	vector3 v3End;
+	std::vector<int> loop;
+	loop.push_back(0);
+	static uint route = 0;
+	static std::vector<uint> routeList;
+	int startSide = 3;
+
+	// create list of static uint value to store for each shape
+	for (int i = 0; i < m_uOrbits;i++)
+	{
+		if (routeList.size() < m_uOrbits)
+		{
+			routeList.push_back(route);
+		}
+
+	}
+
+	// draw a shapes
+	for (uint i = 0; i < m_uOrbits; i++)
+	{
+
+		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 90.0f, AXIS_X));
+
+			//calculate the current position
+			v3Start = stoplist[loop[i] + ((routeList[i]) % (startSide))];
+			v3End = stoplist[loop[i] + ((routeList[i]+1) % (startSide))];
+
+			// calculate current progress
+			float fPercentage = MapValue(fTimer, 0.0f, 1.0f, 0.0f, 1.0f);
+
+			if (fPercentage >= 1.0f)
+			{
+				for (int j = 0;j < m_uOrbits;j++)
+				{
+					routeList[j]++;
+					fTimer = m_pSystem->GetDeltaTime(uClock);
+				}
+
+			}
+			// check if the value exceed side limit
+			routeList[i] %= (startSide);
+
+
+			vector3 v3CurrentPos = glm::lerp(v3Start, v3End, fPercentage);
+			matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
+
+			//draw spheres
+			m_pMeshMngr->AddSphereToRenderList(m4Model * glm::scale(vector3(0.1)), C_WHITE);
+
+			loop.push_back(loop[i] + startSide);
+			startSide++;
+
+
+	}
+
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	//release GUI
+	ShutdownGUI();
+}
